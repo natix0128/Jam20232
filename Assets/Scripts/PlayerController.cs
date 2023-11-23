095949dd925552bff6b80e0f61b13f6a40f0cfb8
natix0128 <127872082+natix0128@users.noreply.github.com>
2023-11-22 20:21:49 -0500

    movimiento+anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,83 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed = 5.0f;
-    public float rotationSpeed = 200.0f;
-    private Animator anim;
-    public float x, y;
-    void Start()
+    private CharacterController controller;
+    private GameObject camara;
+
+    [Header("Estadisticas Normales")]
+    [SerializeField] private float velocidad;
+    [SerializeField] private float velCorriendo;
+    [SerializeField] private float alturaDeSalto;
+    [SerializeField] private float tiempoAlGirar;
+
+    [Header("Datos sobre el suelo")]
+    [SerializeField] private Transform detectaPiso;
+    [SerializeField] private float distanciaPiso;
+    [SerializeField] private LayerMask mascaraPiso;
+
+
+    float velocidadGiro;
+    float gravedad = -9.81f;
+    Vector3 velocity;
+    bool tocaPiso;
+
+    Animator anim;
+
+    private void Start()
     {
-        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        camara = GameObject.FindGameObjectWithTag("MainCamera");
+        anim = GetComponentInChildren<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
+        tocaPiso = Physics.CheckSphere(detectaPiso.position, distanciaPiso, mascaraPiso);
 
-        transform.Rotate(0, x * Time.deltaTime * rotationSpeed, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * movementSpeed);
+        if (tocaPiso && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
 
-        anim.SetFloat("VelX", x);
-        anim.SetFloat("VelY", y);
-    }
+        if (Input.GetButtonDown("Jump") && tocaPiso)
+        {
+            velocity.y = Mathf.Sqrt(alturaDeSalto * -2 * gravedad);
+        }
+
+        velocity.y += gravedad * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direccion = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direccion.magnitude >= 0.1f)
+        {
+            float objetivoAngulo = Mathf.Atan2(direccion.x, direccion.z) * Mathf.Rad2Deg + camara.transform.eulerAngles.y;
+            float angulo = Mathf.SmoothDampAngle(transform.eulerAngles.y, objetivoAngulo, ref velocidadGiro, tiempoAlGirar);
+            transform.rotation = Quaternion.Euler(0, angulo, 0);
+
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                Vector3 mover = Quaternion.Euler(0, objetivoAngulo, 0) * Vector3.forward;
+                controller.Move(mover.normalized * velCorriendo * Time.deltaTime);
+                anim.SetFloat("Blend", 1f, 0.1f, Time.deltaTime);
+            }
+            else
+            {
+                Vector3 mover = Quaternion.Euler(0, objetivoAngulo, 0) * Vector3.forward;
+                controller.Move(mover.normalized * velocidad * Time.deltaTime);
+                anim.SetFloat("Blend", 0.5f, 0.1f, Time.deltaTime);
+            }
+            
+        }
+
+        else
+        {
+            anim.SetFloat("Blend", 0f, 0.1f, Time.deltaTime);
+        }
+    }   
+
+    
+
 }
